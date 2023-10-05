fb52cddcd6293a8d3d8ccaaab82ca74a4d8d21ae
NickSeagull <NickSeagull@users.noreply.github.com>
2023-10-05 21:57:42 +0000

    deploy: a23e54851bd8290b33058c18a7e7dbc99fe1908d

---
+++ assets/js/b2b675dd.a5d16945.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkneohaskell_github_io=self.webpackChunkneohaskell_github_io||[]).push([[533],{8017:e=>{e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"NeoHaskell Evolution Proposals","blogTitle":"NHEP"}')}}]);
